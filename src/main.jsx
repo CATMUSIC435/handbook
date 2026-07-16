442095940a3364559418c00f2ac8e66b10be2a49
phuongdongiot <catmusic435@gmail.com>
2026-07-16 13:02:54 +0700

    Add PWA auto update logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from 'virtual:pwa-register';
+
+// Tự động kiểm tra bản cập nhật mới mỗi 60 phút
+const updateSW = registerSW({
+  onRegistered(r) {
+    r && setInterval(() => {
+      r.update();
+    }, 60 * 60 * 1000); // 1 tiếng
+  },
+  onOfflineReady() {
+    console.log("Ứng dụng đã sẵn sàng hoạt động ngoại tuyến!");
+  }
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
